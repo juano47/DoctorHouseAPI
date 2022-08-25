81ec622fcdd05c9adaefd7109af47f462b06ea52
Juan Ignacio de la Iglesia <juan.delaiglesia.jdli@gmail.com>
2022-08-25 14:12:06 +0200

    omit feature flags functionality

--- src/main/java/com/delaiglesia/doctorhouseapi/aspect/FeatureFlagAspect.java
+++ src/main/java/com/delaiglesia/doctorhouseapi/aspect/FeatureFlagAspect.java
@@ -1,17 +1,10 @@
 package com.delaiglesia.doctorhouseapi.aspect;
 
-import com.delaiglesia.doctorhouseapi.services.FeatureFlagService;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
-
-@Aspect
-@Component
+//@Aspect
+//@Component
 public class FeatureFlagAspect {
 
-    private FeatureFlagService featureFlagService;
+  /*  private FeatureFlagService featureFlagService;
 
     public FeatureFlagAspect(FeatureFlagService featureFlagService) {
         this.featureFlagService = featureFlagService;
@@ -23,5 +16,5 @@ public class FeatureFlagAspect {
         if (!featureFlagService.isFeatureFlagSet(flag)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Feature flag " + flag + " is not enabled");
         }
-    }
+    }*/
 }
--- src/main/java/com/delaiglesia/doctorhouseapi/services/impl/FileFeatureFlagService.java
+++ src/main/java/com/delaiglesia/doctorhouseapi/services/impl/FileFeatureFlagService.java
@@ -2,25 +2,20 @@ package com.delaiglesia.doctorhouseapi.services.impl;
 
 import com.delaiglesia.doctorhouseapi.services.FeatureFlagService;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 
 @Service
 public class FileFeatureFlagService implements FeatureFlagService {
 
     private Properties properties;
-
+/*
     public FileFeatureFlagService() throws IOException {
         properties = new Properties();
         File file = ResourceUtils.getFile("classpath:featuresFlags.properties");
         InputStream stream = new FileInputStream(file);
         properties.load(stream);
-    }
+    }*/
 
     @Override
     public boolean isFeatureFlagSet(String flag) {
